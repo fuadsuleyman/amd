85de8422406c54dbe8fde7cf7142c9b67cb20420
Elvin Husubeyli <husubayli@gmail.com>
2021-02-18 16:34:39 +0400

    sss

--- amdtelecom/product/urls.py
+++ amdtelecom/product/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 # from .views import ProductListView
 from django.conf.urls.static import static
+
+app_name = 'product'
 
 urlpatterns = [
     # path('', views.home, name='amd-home'),
@@ -9,5 +11,6 @@ urlpatterns = [
     # path('', views.home_page, name='amd-home'),
     # path('product/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
     path('product/<int:id>/', views.product_detail, name='product-detail'),
-    path('about/', views.about, name='amd-about'),
+    path('products/', views.about, name='amd-about'),
+    path('api/v1.0/', include('product.api.urls')),
 ]
