0453c90be575b010e2512e997a564989333d30a9
Elvin Husubeyli <husubayli@gmail.com>
2021-02-21 03:30:26 +0400

    fixed admin py

--- amdtelecom/product/admin.py
+++ amdtelecom/product/admin.py
@@ -24,26 +24,11 @@ class CategoryAdmin(admin.ModelAdmin):
         'slug',
     )
 
-# @admin.register(Customer)
-# class CustomerAdmin(admin.ModelAdmin):
-#     list_display = ("id", "device")
-#     list_display_links = ("device",)
-
-# @admin.register(Order)
-# class OrderAdmin(admin.ModelAdmin):
-#     list_display = ("id", "date_ordered")
-#     list_display_links = ("date_ordered",)
-
-# @admin.register(OrderItem)
-# class OrderItemAdmin(admin.ModelAdmin):
-#     list_display = ("id", "quantity", "date_added")
-#     list_display_links = ("quantity",)
 
 @admin.register(Marka)
 class MarkaAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "description")
     list_display_links = ("title",)
-    readonly_fields = ('slug',)
 
 # @admin.register(Product_colors)
 # class ColorpAdmin(admin.ModelAdmin):
@@ -53,40 +38,48 @@ class MarkaAdmin(admin.ModelAdmin):
 class ImageInline(admin.TabularInline):
     model = Product_images
     extra = 0
-class ImageInline(admin.TabularInline):
-    model = Product_images
-    extra = 0
+    
+@admin.register(Product_details_property_name)
+class DetailAdmin(admin.ModelAdmin):
+    list_display = ("title", "status")
 
-# class DetailsInline(admin.t):
-#     model = Product_details
-#     extra = 0
+@admin.register(Product_details_property_value)
+class DetailAdmin(admin.ModelAdmin):
+    list_display = ("title", "status")
 
-class DetailsInline(admin.TabularInline):
-    '''Tabular Inline View for '''
 
+class ProductDetailNameAdmin(admin.TabularInline):
     model = Product_details
-    # min_num = 3
-    # max_num = 20
-    # extra = 1
-    # raw_id_fields = (,)
+    extra = 0
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("title", "price", 'id') #"get_image"
+    list_display = ("title", "price", "is_new",) #"get_image"
     list_display_links = ("title",)
-    list_filter = ("price",)
-    search_fields = ('title', "category__name")
-    inlines = [ImageInline, DetailsInline]
+    list_filter = ("price", "category",)
+    search_fields = ('title', "category__title", "Marka")
+    inlines = [ImageInline, ProductDetailNameAdmin]
     save_on_top = True
     save_as = True #create new product easy way
 
+    fieldsets = (
+        ('Relations', {
+            'fields': ('category','marka', 'tags', 'same_product'),
+        }),
+        ('Informations', {
+            'fields': (('title', 'slug'), 'sku', ('color_title', 'color_code',), 'description', 'sale_count', ('is_new', 'is_featured', 'is_discount'), 'status')
+        }),
+        ('Price Info', {
+            'fields': ('price', 'discount_type', 'discount_value'),
+        }),
+    )
+
     # def get_image(self, obj):
-    #     return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+    #     return mark_safe(f'<img src={obj.images.get(is_main=True).imageURL} width="50" height="60"')
+
 
     # get_image.short_description = "Image"
-    # get_image.allow_tags = True
-
+    
 
 admin.site.register(Tag)
-admin.site.register(Product_images)
-admin.site.register([Product_details, Product_details_property_name, Product_details_property_value])
+admin.site.register([Product_images, ])
