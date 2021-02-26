51d907cc9649653562be47aab938cfdf45fdb2e4
koki014 <koki.suleymanov@gmail.com>
2021-02-26 17:01:58 +0400

    for push

--- amdtelecom/contact/views.py
+++ amdtelecom/contact/views.py
@@ -6,6 +6,7 @@ from django.views.generic.edit import (
 from .models import Contact
 from .forms import ContactForm
 from django.contrib.messages import success
+from django.shortcuts import redirect
 
 
 
@@ -13,7 +14,7 @@ class ContactCreateView(CreateView):
     model = Contact
     form_class = ContactForm
     template_name = "contact.html"
-    
+
     def form_valid(self, form):
         success(self.request, 'Mesajiniz qeyde alinmisdir tez bir zamanda sizinle elaqe saxlanilicaq.')
         # success_url = reverse_lazy('index:home')
