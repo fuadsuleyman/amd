efcc8510edb574f38d55c1f2af88b1bc5a59dbe0
Elvin Husubeyli <husubayli@gmail.com>
2021-03-01 15:19:29 +0400

    completed filter api data main image and second image return defaul or no

--- amdtelecom/product/api/views.py
+++ amdtelecom/product/api/views.py
@@ -31,76 +31,41 @@ class ProductFilterListAPIView(ListAPIView):
         category = self.request.GET.get('category')
 
         products = Product.objects.filter(category=category)
+        # products = Product.objects.filter(is_published=True)
         
         color_title = self.request.GET.getlist('color_title[]')
         is_new = self.request.GET.getlist('is_new[]')
         marka = self.request.GET.getlist('marka[]')
         min_price = self.request.GET.get('price_min')
         max_price = self.request.GET.get('price_max')
-        print(len(min_price), 'balaca')
-        # if min_price != '':
-        #     print(type(max_price), 'boyuk_olcu')
-        # if min_price or max_price:
-        #     products = products.filter(price__range=(min_price, max_price) or None)
-        #     print(products, 'salam')
-        #     # print(products.objects.none())
-        #     if products.count() == 0:
-        #         print('productun say serti', products.count())
-        #         result = False
-            # result = False
-            # for item in products:
-            #     print(item.price)
-            #     if min_price:
-            #         print('girdi ----- bura'*10)
-            #         print(int(min_price))
-            #         print(int(item.price))
-            #         print(int(max_price))
-            #         products = products.filter(price__range=(min_price, max_price))
-            #         print(products)
-            #         if int(min_price) <= int(item.price) and int(item.price) <= int(max_price):
-            #             products = products.filter(price__range=(min_price, max_price))
-            #             print(products, 'salam necesen kele')
-            #             result = True
-            #         elif int(max_price) == int(min_price):
-            #             print('beraberdir')
-            #             products = products.filter(price=min_price)
-            #             result == True
-            #         elif min_price == '':
-            #             print('salam necesen')
-            #             result = True
 
-        print('resulta girdi')
-
-        
-        if products.count() > 0:
-            if color_title:
-                if min_price:
-                    products = products.filter(color_title__in=color_title).filter(price__range=(min_price, max_price) or None)
-                else:
-                    products = products.filter(color_title__in=color_title or None).distinct()
-                print(products)
+        if color_title:
+            if min_price:
+                products = products.filter(color_title__in=color_title).filter(price__range=(min_price, max_price) or None)
             else:
-                if min_price:
-                    products = products.filter(price__range=(min_price, max_price) or None)
-            if is_new:
-                if min_price:
-                    products = products.filter(is_new=is_new[0]).filter(price__range=(min_price, max_price) or None)
-                else:
-                    products = products.filter(is_new=is_new[0] or None)
-            else:
-                if min_price:
-                    products = products.filter(price__range=(min_price, max_price) or None)
-            if marka:
-                if min_price:
-                    products = products.filter(marka__id__in=marka).filter(price__range=(min_price, max_price) or None)
-                else:
-                    products = products.filter(marka__id__in=marka or None).distinct()
-                    print(products, 'markasi')
-            else:
-                if min_price:
-                    products = products.filter(price__range=(min_price, max_price) or None)
+                products = products.filter(color_title__in=color_title or None).distinct()
+            print(products)
         else:
-            products = {}
+            if min_price:
+                products = products.filter(price__range=(min_price, max_price) or None)
+        if is_new:
+            if min_price:
+                products = products.filter(is_new=is_new[0]).filter(price__range=(min_price, max_price) or None)
+            else:
+                products = products.filter(is_new=is_new[0] or None)
+        else:
+            if min_price:
+                products = products.filter(price__range=(min_price, max_price) or None)
+        if marka:
+            if min_price:
+                products = products.filter(marka__id__in=marka).filter(price__range=(min_price, max_price) or None)
+            else:
+                products = products.filter(marka__id__in=marka or None).distinct()
+                print(products, 'markasi')
+        else:
+            if min_price:
+                products = products.filter(price__range=(min_price, max_price) or None)
+                
         return products
 
 
--- amdtelecom/static/js/product-filter.js
+++ amdtelecom/static/js/product-filter.js
@@ -30,6 +30,8 @@ $(document).on('input', '#range', function() {
 
 function getProImage(id){
     let image	
+    let mainImage, secondImage
+    let filterProductImage = {}
     $.ajax({
         url: `${domain}api/v1.0/filter-api-product-images/`,
         // data: _filterObj,
@@ -39,13 +41,40 @@ function getProImage(id){
         
         success:function(res){
             console.log(res);
-            for(j of res){
+            for(let item of res){
                 // console.log(j);
                 // console.log(j.id);
-                if (j.id == id ) {
-                    image = j.image
+                if (item.id == id ) {
+                    image = item.image
+                    // console.log(item.is_main, 'main');
+                    // console.log(item.is_second_main, 'second');
+                    // if(item.is_main){
+                    //     mainImage = item.image
+                    //     return mainImage
+                    //     console.log(mainImage, 'main image');
+                    // }
+                    // if(item.is_second_main){
+                    //     secondImage = item.image
+                    //     return secondImage
+                    //     console.log(secondImage, 'second image');
+                    // }
+                    
+                    // console.log(item.is_main, 'salam');
+                
                 }
             }
+            // console.log(secondImage, 'second');
+            // console.log(mainImage, 'main');
+            // if (mainImage != undefined){
+            //     filterProductImage['mainImage'] = mainImage
+            //     return filterProductImage.mainImage
+            // }
+            // else if (secondImage != undefined) {
+            //     filterProductImage['secondImage'] = secondImage
+            //     return filterProductImage.secondImage
+            // }
+            // console.log(filterProductImage.mainImage);
+            // console.log(filterProductImage.secondImage);
         },
         error: function(res){
             console.log(res, 'error');
@@ -62,29 +91,36 @@ function getData() {
     $(".ajaxLoader").hide();
 
     var _filterObj={};
-    $(".filter-item-checkbox").each(function(index,ele){
+    _filterObj['price_min'] = min
+    _filterObj['price_max'] = max
+    _filterObj['category'] = theCategory
+    if($(".filter-item-checkbox")){
+        $(".filter-item-checkbox").each(function(index,ele){
+            _filterObj['price_min'] = min
+            _filterObj['price_max'] = max
+            var _filterVal=$(this).val();
+            var _filterKey=$(this).data('filter');
 
-        var _filterVal=$(this).val();
-        var _filterKey=$(this).data('filter');
+            console.log(min);
+            // if (min == ''){
+            // }
+            _filterObj[_filterKey]=Array.from(document.querySelectorAll('input[data-filter='+_filterKey+']:checked')).map(function(el){
+                return el.value;
+            })
 
-        console.log(min);
-        // if (min == ''){
-        _filterObj['price_min'] = min
-        _filterObj['price_max'] = max
-        // }
-        _filterObj['category'] = theCategory
-        _filterObj[_filterKey]=Array.from(document.querySelectorAll('input[data-filter='+_filterKey+']:checked')).map(function(el){
-            return el.value;
-        })
+            console.log(_filterObj);
 
-        console.log(_filterObj);
-
-    });
+        });
+    }
+    // else{
+    //     log
+    // }
+    console.log(_filterObj, 'yeni');
 
     $.ajax({
         url: `${domain}api/v1.0/filter-api-product/`,
         type : 'GET',
-        data:_filterObj,
+        data: _filterObj,
         dataType:'json',
         success:function(response){
             console.log(response, 'product data');
@@ -93,23 +129,64 @@ function getData() {
             if (response.length > 0){  
 
                 for(let product of response){
-                    
-                    products += `
+                    console.log(product, 'datalar');
+                    if (product.color_code != null){
+                        if (product.images.length > 1){
+                            products += `
+                                    <div class="col-xl-3 col-6 col-grid-box">
+                                    <div class="product-box">
+                                        <div class="img-wrapper">
+                                            <div class="front">
+                                                    <a href="http://localhost:8000/product/${product.slug}/">
+                                                    <img
+                                                        src="${getProImage(product.images[1])}"
+                                                        class="img-fluid blur-up lazyload bg-img" alt="${product.title}" style="height: 271px;">
+                                                    </a>
+                                            </div>
+                                            <div class="back">
+                                                <a href="http://localhost:8000/product/${product.slug}/">
+                                                    <img
+                                                        src="${getProImage(product.images[0])}"
+                                                        class="img-fluid blur-up lazyload bg-img" alt="${product.title}" style="height: 271px;">
+                                                    </a>
+                                            </div>
+                                            <div class="cart-info cart-wrap">
+                                                <button data-toggle="modal" data-target="#addtocart" title="Add to cart"><i
+                                                        class="ti-shopping-cart"></i></button> <a href="javascript:void(0)" title="Add to Wishlist"><i
+                                                        class="ti-heart" aria-hidden="true"></i></a> <a href="#" data-toggle="modal" data-target="#quick-view" title="Quick View"><i
+                                                        class="ti-search" aria-hidden="true"></i></a> <a href="compare.html" title="Compare"><i
+                                                        class="ti-reload" aria-hidden="true"></i></a>
+                                            </div>
+                                        </div>
+                                        <div class="product-detail">
+                                            <div>
+                                                <a href="product-page(no-sidebar).html">
+                                                    <h6>${product.title}</h6>
+                                                </a>
+                                                <p>${product.description}
+                                                </p>
+                                                <h4>${product.price} AZN</h4>
+                                                <ul class="color-variant">
+                                                    <li class="bg-light0" style="background-color: ${product.color_code} !important;"></li>
+                                                    <!-- <li class="bg-light1"></li>
+                                                    <li class="bg-light2"></li> -->
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            `
+                        }
+                        else{
+                            products += `
                             <div class="col-xl-3 col-6 col-grid-box">
                             <div class="product-box">
                                 <div class="img-wrapper">
                                     <div class="front">
                                             <a href="http://localhost:8000/product/${product.slug}/">
                                             <img
-                                                src="${getProImage(product.images[1])}"
-                                                class="img-fluid blur-up lazyload bg-img" alt="">
-                                            </a>
-                                    </div>
-                                    <div class="back">
-                                        <a href="http://localhost:8000/product/${product.slug}/">
-                                            <img
                                                 src="${getProImage(product.images[0])}"
-                                                class="img-fluid blur-up lazyload bg-img" alt="${product.title}">
+                                                class="img-fluid blur-up lazyload bg-img" alt="${product.title}" style="height: 271px;">
                                             </a>
                                     </div>
                                     <div class="cart-info cart-wrap">
@@ -138,7 +215,85 @@ function getData() {
                             </div>
                         </div>
                     `
-
+                        }
+                    }
+                    else {
+                        if (product.images.length > 1){
+                            products += `
+                                    <div class="col-xl-3 col-6 col-grid-box">
+                                    <div class="product-box">
+                                        <div class="img-wrapper">
+                                            <div class="front">
+                                                    <a href="http://localhost:8000/product/${product.slug}/">
+                                                    <img
+                                                        src="${getProImage(product.images[1])}"
+                                                        class="img-fluid blur-up lazyload bg-img" alt="${product.title}" style="height: 271px;">
+                                                    </a>
+                                            </div>
+                                            <div class="back">
+                                                <a href="http://localhost:8000/product/${product.slug}/">
+                                                    <img
+                                                        src="${getProImage(product.images[0])}"
+                                                        class="img-fluid blur-up lazyload bg-img" alt="${product.title}" style="height: 271px;">
+                                                    </a>
+                                            </div>
+                                            <div class="cart-info cart-wrap">
+                                                <button data-toggle="modal" data-target="#addtocart" title="Add to cart"><i
+                                                        class="ti-shopping-cart"></i></button> <a href="javascript:void(0)" title="Add to Wishlist"><i
+                                                        class="ti-heart" aria-hidden="true"></i></a> <a href="#" data-toggle="modal" data-target="#quick-view" title="Quick View"><i
+                                                        class="ti-search" aria-hidden="true"></i></a> <a href="compare.html" title="Compare"><i
+                                                        class="ti-reload" aria-hidden="true"></i></a>
+                                            </div>
+                                        </div>
+                                        <div class="product-detail">
+                                            <div>
+                                                <a href="product-page(no-sidebar).html">
+                                                    <h6>${product.title}</h6>
+                                                </a>
+                                                <p>${product.description}
+                                                </p>
+                                                <h4>${product.price} AZN</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            `
+                        }
+                        else{
+                            products += `
+                            <div class="col-xl-3 col-6 col-grid-box">
+                            <div class="product-box">
+                                <div class="img-wrapper">
+                                    <div class="front">
+                                            <a href="http://localhost:8000/product/${product.slug}/">
+                                            <img
+                                                src="${getProImage(product.images[0])}"
+                                                class="img-fluid blur-up lazyload bg-img" alt="${product.title}" style="height: 271px;">
+                                            </a>
+                                    </div>
+                                    <div class="cart-info cart-wrap">
+                                        <button data-toggle="modal" data-target="#addtocart" title="Add to cart"><i
+                                                class="ti-shopping-cart"></i></button> <a href="javascript:void(0)" title="Add to Wishlist"><i
+                                                class="ti-heart" aria-hidden="true"></i></a> <a href="#" data-toggle="modal" data-target="#quick-view" title="Quick View"><i
+                                                class="ti-search" aria-hidden="true"></i></a> <a href="compare.html" title="Compare"><i
+                                                class="ti-reload" aria-hidden="true"></i></a>
+                                    </div>
+                                </div>
+                                <div class="product-detail">
+                                    <div>
+                                        <a href="product-page(no-sidebar).html">
+                                            <h6>${product.title}</h6>
+                                        </a>
+                                        <p>${product.description}
+                                        </p>
+                                        <h4>${product.price} AZN</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `
+                        }
+                    }
                     DOM.html(products)
                     
 
