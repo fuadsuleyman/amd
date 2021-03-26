d204713991818e91539b2d79498a9c7f01852b61
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-03-26 16:23:32 +0400

    some small changes

--- amdtelecom/amdtelecom/settings.py
+++ amdtelecom/amdtelecom/settings.py
@@ -226,11 +226,7 @@ else:
         BASE_DIR / "static",
     ]
     
-
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
-
+    
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
