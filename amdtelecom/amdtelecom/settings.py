0d518b996a66f5cc3f359cdc0a9c44efc755a591
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-05-03 09:05:23 +0400

    removed mathfilters from installed apps

--- amdtelecom/amdtelecom/settings.py
+++ amdtelecom/amdtelecom/settings.py
@@ -37,7 +37,6 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'jet.dashboard',
     'jet',
-    'mathfilters',
     'contact.apps.ContactConfig',
     'product.apps.ProductConfig',
     'account.apps.AccountConfig',
