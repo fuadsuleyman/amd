aa1d69484e8659f96e7c63c6b0672ad33eec3697
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-05-19 15:21:41 +0400

    add banners in home page

--- amdtelecom/index/models.py
+++ amdtelecom/index/models.py
@@ -43,14 +43,20 @@ class Logo(models.Model):
         return f'{self.title}'
 
 
-class SlideInfo(models.Model):
+class Banner(models.Model):
     # informations
-    title = models.CharField('Title', max_length=100, db_index=True)
-    description = models.CharField(max_length=255, blank=True)
-    image = models.ImageField('Slide Image', upload_to='slide_pics', null=True, blank=True)
-    is_main = models.BooleanField('Main Slider', default=False)
-    is_full_screen = models.BooleanField('Full Screen', default=False)
-    video = models.CharField('Title', max_length=100, db_index=True, null=True, blank=True)
+    title = models.CharField('Title', max_length=100, blank=True, null=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    link = models.CharField('Link', max_length=300, blank=True, null=True)
+    image = models.ImageField('Image', upload_to='banner_pics', null=True, blank=True)
+    is_main = models.BooleanField('Main Image', default=False)
+    is_second_yuxari = models.BooleanField('Second yuxari', default=False)
+    is_second_ashagi = models.BooleanField('Second asagi', default=False)
+    is_third_sol = models.BooleanField('Third sol', default=False)
+    is_third_sag = models.BooleanField('Third sag', default=False)
+    is_third_orta = models.BooleanField('Third orta', default=False)
+    # is_full_screen = models.BooleanField('Full Screen', default=False)
+    # video = models.CharField('Title', max_length=100, db_index=True, null=True, blank=True)
      
     # moderations
     status = models.BooleanField('Status', default=True)
@@ -58,13 +64,21 @@ class SlideInfo(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = 'slide_infp'
-        verbose_name = 'Slide_info'
-        verbose_name_plural = 'Slide_infos'
+        db_table = 'banner'
+        verbose_name = 'Banner'
+        verbose_name_plural = 'Banners'
         ordering = ('created_at',)
 
     def __str__(self):
         return f'{self.title}'
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 
 class ParallaxBanner(models.Model):
--- amdtelecom/index/views.py
+++ amdtelecom/index/views.py
@@ -1,10 +1,12 @@
 from product.models import Product
+from .models import Banner
 from order.models import Order
 from account.models import Customer
 from product.models import Product_details, Category
 from django.views.generic import TemplateView
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
 
 
 # class HomePageTemplateView(TemplateView):
@@ -36,10 +38,21 @@ def home_page(request):
     products = Product.objects.all()
     category = Category.objects.all()
     details = Product_details.objects.all()
+    banner_main = Banner.objects.get(is_main=True)
+    banner_second_yuxari = Banner.objects.get(is_second_yuxari=True)
+    banner_second_ashagi = Banner.objects.get(is_second_ashagi=True)
+    banner_third_sol = get_object_or_404(Banner, is_third_sol=True)
+    # banner_third_sol = Banner.objects.get(is_third_sol=True)
+    banner_third_sag = get_object_or_404(Banner, is_third_sag=True)
+    # banner_third_sag = Banner.objects.get(is_third_sag=True)
+    banner_third_orta = get_object_or_404(Banner, is_third_orta=True)
+    # banner_third_orta = Banner.objects.get(is_third_orta=True)
     # for home page return filter products
     new_arrivals = products.filter(is_published=True).filter(is_new=True).order_by('-created_at')[:12]
     most_sold = products.filter(is_published=True).order_by('-sale_count')[:12]
     discounted_products = products.filter(is_published=True).filter(is_discount=True).order_by('-created_at')[:12]
+
+    print("Main banner image", banner_main.image)
 
     context = {
         'products': products, 
@@ -49,7 +62,13 @@ def home_page(request):
         'order': order, 
         'new_arrivals': new_arrivals, 
         'discounted_products': discounted_products, 
-        'most_sold': most_sold
+        'most_sold': most_sold,
+        'banner_main': banner_main,
+        'banner_second_yuxari': banner_second_yuxari,
+        'banner_second_ashagi': banner_second_ashagi,
+        'banner_third_sol': banner_third_sol,
+        'banner_third_sag':banner_third_sag,
+        'banner_third_orta': banner_third_orta
     }
     
     return render(request, 'home.html', context)
